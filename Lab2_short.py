cc01bc5b444dd8e63f27cd4632c70abe12da3d61
Tim Johansson <70037675+ejctim@users.noreply.github.com>
2024-11-20 22:22:38 +0100

    Update Lab2_short.py
    More short

--- Lab2_short.py
+++ Lab2_short.py
@@ -1,5 +1,3 @@
 from sense_hat import SenseHat; s=SenseHat()
 def arrow(): W, B = [255, 255, 255], [0, 0, 0]; return [B]*3 + [W]*2 + [B]*5 + [W]*4 + [B]*3 + [W]*6 + ([B] + [W]*2)*3 + [B]*3 + ([W]*2 + [B]*6)*3 + [W]*2 + [B]*3
-while True: 
-  for e in s.stick.get_events(): 
-    if e.action == "pressed": s.clear() if e.direction == "middle" else s.set_rotation({"down": 180, "left": 270, "right": 90}.get(e.direction, 0)) or s.set_pixels(arrow())
+while True: [s.clear() if e.direction == "middle" else s.set_rotation({"down": 180, "left": 270, "right": 90}.get(e.direction, 0)) or s.set_pixels(arrow()) for e in s.stick.get_events() if e.action == "pressed"]
