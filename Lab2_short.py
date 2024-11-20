2fceda510c2ea18c947b5b45cff7906a33b76255
Tim Johansson <70037675+ejctim@users.noreply.github.com>
2024-11-20 22:00:49 +0100

    Update Lab2_short.py

--- Lab2_short.py
+++ Lab2_short.py
@@ -5,4 +5,4 @@ def arrow():
     return [B]*3 + [W]*2 + [B]*5 + [W]*4 + [B]*3 + [W]*6 + ([B] + [W]*2)*3 + [B]*3 + ([W]*2 + [B]*6)*3 + [W]*2 + [B]*3
 while True: 
   for e in s.stick.get_events(): 
-    if e.action == "pressed": s.clear() if e.direction == "middle" else s.set_rotation({"up": 0, "down": 180, "left": 270, "right": 90}.get(e.direction, 0)) or s.set_pixels(arrow())
+    if e.action == "pressed": s.clear() if e.direction == "middle" else s.set_rotation({"down": 180, "left": 270, "right": 90}.get(e.direction, 0)) or s.set_pixels(arrow())
