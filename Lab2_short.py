b0acb65fb10cad282eb5a441d9087290f1ed40d1
Tim Johansson <70037675+ejctim@users.noreply.github.com>
2024-11-20 22:51:13 +0100

    Update Lab2_short.py

--- Lab2_short.py
+++ Lab2_short.py
@@ -1,2 +1,2 @@
-from sense_hat import SenseHat; s=SenseHat();  W, B = [255, 255, 255], [0, 0, 0];
+from sense_hat import SenseHat; s=SenseHat();  W, B = [255, 255, 255], [0, 0, 0]
 while True: [s.clear() if e.direction == "middle" else s.set_rotation({"down": 180, "left": 270, "right": 90}.get(e.direction, 0)) or s.set_pixels([B]*3 + [W]*2 + [B]*5 + [W]*4 + [B]*3 + [W]*6 + ([B] + [W]*2)*3 + [B]*3 + ([W]*2 + [B]*6)*3 + [W]*2 + [B]*3) for e in s.stick.get_events() if e.action == "pressed"]
